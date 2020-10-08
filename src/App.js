378d78e9ba68abf2abb1d171b9dab93c02f693ba
chaikovskyit <chaikovskyit@gmail.com>
2020-10-08 11:37:54 +0300

    learn create component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Car from './Car/Car.jsx';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const divStyle = {
+		textAlign: 'center',
+	};
+
+	return (
+		<div style={divStyle}>
+			<h1>Hello world</h1>
+			<Car />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Car/Car.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// example 1
+// function Car(){
+//   return(
+//     <div>This is car component</div>
+//   )
+// }
+
+// example 2
+// const Car = () => {
+// 	return <div>This is car component</div>;
+// };
+
+// example 3
+// const Car = () => <div>This is car component</div>;
+
+// export default Car;
+
+// example 4
+export default () => (
+	<div>
+		This is car component
+		<strong>test</strong>
+	</div>
+);
