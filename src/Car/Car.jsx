3a8bbd89677cc86b8a2f94858e1e0d75be2fead7
chaikovskyit <chaikovskyit@gmail.com>
2020-10-08 11:47:20 +0300

    output dynamic data

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-// example 1
-// function Car(){
-//   return(
-//     <div>This is car component</div>
-//   )
-// }
 
-// example 2
-// const Car = () => {
-// 	return <div>This is car component</div>;
-// };
-
-// example 3
-// const Car = () => <div>This is car component</div>;
-
-// export default Car;
-
-// example 4
 export default () => (
 	<div>
-		This is car component
-		<strong>test</strong>
+		<p>This is car component </p>
+		<p>
+			<strong>Number: {Math.round(Math.random() * 100)}</strong>
+		</p>
 	</div>
 );
