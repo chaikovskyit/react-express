559452cfe5c8107f33339d0fcb3629cb49e2fa7d
chaikovskyit <chaikovskyit@gmail.com>
2020-10-09 18:24:37 +0300

    transfer props to component

--- src/App.js
+++ src/App.js
@@ -67,7 +67,8 @@ class App extends Component {
 
 		return (
 			<div style={divStyle}>
-				<h1>{this.state.pageTitle}</h1>
+				{/* <h1>{this.state.pageTitle}</h1> */}
+				<h1>{this.props.title}</h1>
 				<button onClick={this.toggleCarsHandler}>Toggle</button>
 				<div
 					style={{
