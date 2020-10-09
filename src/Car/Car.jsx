e04307987292b60b0918017d2b7b3d76f7abaaac
chaikovskyit <chaikovskyit@gmail.com>
2020-10-09 21:51:07 +0300

    refactring change component Car

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -1,34 +1,65 @@
 import React from 'react';
 import './Car.css';
 
-export default (props) => {
-	const inputClasses = ['input'];
+class Car extends React.Component {
+	render() {
+		const inputClasses = ['input'];
 
-	// if (props.name !== '') {
-	// 	inputClasses.push('green');
-	// } else {
-	// 	inputClasses.push('red');
-	// }
-	props.name !== '' ? inputClasses.push('green') : inputClasses.push('red');
+		this.props.name !== '' ? inputClasses.push('green') : inputClasses.push('red');
 
-	if (props.name.length > 4) {
-		inputClasses.push('bold');
+		if (this.props.name.length > 4) {
+			inputClasses.push('bold');
+		}
+
+		return (
+			<div className='Car'>
+				<h3>Car name: {this.props.name}</h3>
+				<p>
+					Year: <strong>{this.props.year}</strong>
+				</p>
+				<input
+					type='text'
+					onChange={this.props.onChangeName}
+					value={this.props.name}
+					className={inputClasses.join(' ')}
+				/>
+				<button onClick={this.props.onChangeTitle}>Click</button>
+				<button onClick={this.props.onDelete}>Delete</button>
+			</div>
+		);
 	}
+}
 
-	return (
-		<div className='Car'>
-			<h3>Car name: {props.name}</h3>
-			<p>
-				Year: <strong>{props.year}</strong>
-			</p>
-			<input
-				type='text'
-				onChange={props.onChangeName}
-				value={props.name}
-				className={inputClasses.join(' ')}
-			/>
-			<button onClick={props.onChangeTitle}>Click</button>
-			<button onClick={props.onDelete}>Delete</button>
-		</div>
-	);
-};
+// const Car = (props) => {
+// 	const inputClasses = ['input'];
+
+// 	// if (props.name !== '') {
+// 	// 	inputClasses.push('green');
+// 	// } else {
+// 	// 	inputClasses.push('red');
+// 	// }
+// 	props.name !== '' ? inputClasses.push('green') : inputClasses.push('red');
+
+// 	if (props.name.length > 4) {
+// 		inputClasses.push('bold');
+// 	}
+
+// 	return (
+// 		<div className='Car'>
+// 			<h3>Car name: {props.name}</h3>
+// 			<p>
+// 				Year: <strong>{props.year}</strong>
+// 			</p>
+// 			<input
+// 				type='text'
+// 				onChange={props.onChangeName}
+// 				value={props.name}
+// 				className={inputClasses.join(' ')}
+// 			/>
+// 			<button onClick={props.onChangeTitle}>Click</button>
+// 			<button onClick={props.onDelete}>Delete</button>
+// 		</div>
+// 	);
+// };
+
+export default Car;
