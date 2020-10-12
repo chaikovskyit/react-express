472b2f98cab2bc3a98b2b6049f1e318dac6a32ad
chaikovskyit <chaikovskyit@gmail.com>
2020-10-12 10:45:30 +0300

    refactoring

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -3,11 +3,6 @@ import './Car.css';
 
 class Car extends React.Component {
 	render() {
-		console.log('Car render');
-		// if (Math.random() > 0.7) {
-		// 	throw new Error('Car random failed');
-		// }
-
 		const inputClasses = ['input'];
 
 		this.props.name !== '' ? inputClasses.push('green') : inputClasses.push('red');
@@ -34,37 +29,5 @@ class Car extends React.Component {
 		);
 	}
 }
-
-// const Car = (props) => {
-// 	const inputClasses = ['input'];
-
-// 	// if (props.name !== '') {
-// 	// 	inputClasses.push('green');
-// 	// } else {
-// 	// 	inputClasses.push('red');
-// 	// }
-// 	props.name !== '' ? inputClasses.push('green') : inputClasses.push('red');
-
-// 	if (props.name.length > 4) {
-// 		inputClasses.push('bold');
-// 	}
-
-// 	return (
-// 		<div className='Car'>
-// 			<h3>Car name: {props.name}</h3>
-// 			<p>
-// 				Year: <strong>{props.year}</strong>
-// 			</p>
-// 			<input
-// 				type='text'
-// 				onChange={props.onChangeName}
-// 				value={props.name}
-// 				className={inputClasses.join(' ')}
-// 			/>
-// 			<button onClick={props.onChangeTitle}>Click</button>
-// 			<button onClick={props.onDelete}>Delete</button>
-// 		</div>
-// 	);
-// };
 
 export default Car;
